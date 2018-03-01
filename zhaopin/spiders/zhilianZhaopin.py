05c3eccbe7772775ca914a3c32fe87104b77477f
kumanxuan <775121173@qq.com>
2018-03-01 23:20:54 +0800

    添加一些需求,捉取特定字段,目标是智联招聘.!!

--- zhaopin/spiders/zhilianZhaopin.py
+++ zhaopin/spiders/zhilianZhaopin.py
@@ -13,13 +13,68 @@ class ZhilianzhaopinSpider(CrawlSpider):
 	start_urls = ['https://sou.zhaopin.com/jobs/searchresult.ashx?jl=%E5%B9%BF%E4%B8%9C&kw=java&sm=0&p=1']
 	rules = (Rule(LinkExtractor(),callback='afterProcess',),)
 
+	def processingThisPage(self):
+		pass
 
 	def afterProcess(self,response):
-		#print("hello world!!")
 		title = response.xpath("//title/text()").extract()[0]
 		items = ZhaopinItem()
 		items['title'] = title
+		items['url'] = response.url
+
+		items['title'] = title
+		# 1职位要求
+		items['jobName'] = title
+		# 2公司名
+		items['company'] = title
+		# 3工作地点
+		items['location'] = title
+		# 4学历要求
+		items['backGroup'] = title
+		# 5薪资
+		items['salary'] = title
+		# 6公司规模
+		items['scale'] = title
+		# 7任职要求
+		items['require'] = title
+		# 8任职经验
+		items['experience'] = title
+		# 9企业性质
+		items['enterprise'] = title
 
 		yield items
 	#def parse(self, response):
 		#pass
+	def getJobName(self):
+		pass
+
+
+	def getCompany(self):
+		pass
+
+
+	def getLocation(self):
+		pass
+
+	def getBackGroup(self):
+		pass
+
+
+	def getSalary(self):
+		pass
+
+
+	def getScale(self):
+		pass
+
+	def getRequire(self):
+		pass
+
+
+	def getExperience(self):
+		pass
+
+
+	def getEnterprise(self):
+		pass
+
